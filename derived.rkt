03abb98b84180b74e02f90ffebe123b618b8c103
Phil Nguyen <pcn@cs.umd.edu>
2016-07-21 14:21:38 -0400

    `define-const`

--- derived.rkt
+++ derived.rkt
@@ -1,6 +1,9 @@
 #lang racket/base
 
-(require "parser.rkt"
+(require (for-syntax racket/base
+                     syntax/parse)
+         syntax/parse/define
+         "parser.rkt"
          "builtins.rkt")
 
 ;; Functions that are written in terms of the base functions in main.rkt and
@@ -9,19 +12,19 @@
 ;; Define a function using universal quantifiers as a sort of macro.
 ;; Note that defining recursive functions is possible but highly
 ;; recommended against.
-(define-syntax define-fun
-  (syntax-rules ()
-    [(_ id () type body)
-     (begin
-       (smt:declare-fun id () type)
-       (smt:assert (=/s id body)))]
-    [(_ id ((argname argtype) ...) type body)
-     (begin
-       (smt:declare-fun id (argtype ...) type)
-       (smt:assert (forall/s ((argname argtype) ...)
-                             (=/s (id argname ...) body))))]))
+(define-syntax (define-fun stx)
+  (syntax-parse stx
+    [(_ c:id () T e)
+     #'(begin
+       (smt:declare-const c () T)
+       (smt:assert (=/s c e)))]
+    [(_ f:id ([x:id Tx] ...) T e)
+     #'(begin
+       (smt:declare-fun f (Tx ...) T)
+       (smt:assert (∀/s ([x Tx] ...) (=/s (f x ...) e))))]))
+(define-simple-macro (define-const c:id T e) (define-fun c:id () T e))
 
 (provide
  (prefix-out
   smt:
-  (combine-out define-fun)))
+  (combine-out define-fun define-const)))
