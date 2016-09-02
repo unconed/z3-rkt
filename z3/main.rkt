05266fdd8ff917530ce66eaafa1c569c21b12e38
Phil Nguyen <pcn@cs.umd.edu>
2016-09-02 17:42:02 -0400

    undo hack didn't work

--- z3/main.rkt
+++ z3/main.rkt
@@ -2,8 +2,3 @@
 
 (require "ffi.rkt" "smt.rkt")
 (provide (all-from-out "ffi.rkt" "smt.rkt"))
-
-;; Hack just because I don't know how to run tests on `raco pkg install`
-(require (for-syntax racket/base))
-(begin-for-syntax
-  (require "tests/guide.rkt"))
