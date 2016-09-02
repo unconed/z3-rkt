7e294fcbd15eef20c51160277ea4e58cc42d2ed5
Phil Nguyen <pcn@cs.umd.edu>
2016-09-02 12:59:11 -0400

    dependency

--- z3/info.rkt
+++ z3/info.rkt
@@ -2,6 +2,10 @@
 
 (define collection "z3")
 
+(define deps '("base"
+               "typed-racket-lib"
+               "typed-racket-more"))
+
 (define pkg-desc "Z3 bindings")
 
 (define pkg-authors '(pcn))
